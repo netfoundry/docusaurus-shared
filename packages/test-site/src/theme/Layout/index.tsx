d74b645b53fd57cb1a3676092236e78d7683b6ee
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-07-17 12:59:27 -0400

    checkpoint

--- packages/test-site/src/theme/Layout/index.tsx
+++ packages/test-site/src/theme/Layout/index.tsx
@@ -1,6 +1,5 @@
 import React, {type ReactNode} from 'react';
 import { OpenZitiLayout, OpenZitiLayoutProps } from '@openclint/docusaurus-shared';
-import styles from './styles.module.css'
 
 export default function LayoutWrapper(props: OpenZitiLayoutProps): ReactNode {
     return (
