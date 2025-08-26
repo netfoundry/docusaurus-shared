6a7f4d4f2a3b0fbbbfd71c732dedcb5de72c9b2b
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-08-26 16:43:16 -0400

    trailing slash and client redir

--- unified-doc/docusaurus.config.ts
+++ unified-doc/docusaurus.config.ts
@@ -66,6 +66,7 @@ const config: Config = {
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/docs',
+    trailingSlash: false,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
@@ -162,6 +163,9 @@ const config: Config = {
             },
         ],
         ['@docusaurus/plugin-sitemap', { changefreq: "daily", priority: 0.8 }],
+        ['@docusaurus/plugin-client-redirects', {
+            createRedirects: (path) => path.endsWith('/') ? [path.slice(0, -1)] : [`${path}/`],
+        }],
     ],
     themeConfig: {
         // Replace with your project's social card
