45de9e20171411b570b21561bbe95737e4beff6c
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-08-29 08:24:16 -0400

    update to latest

--- unified-doc/publish-unified-doc.sh
+++ unified-doc/publish-unified-doc.sh
@@ -8,13 +8,13 @@ pub_script_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 echo "publish script located in: $pub_script_root"
 
 publish_docs() {
-  local HOST=$1 PORT=$2 USER=$3 TARGET_DIR=$4 BUILD_DIR=$5
+  local HOST=$1 PORT=$2 USER=$3 TARGET_DIR=$4 BUILD_QUALIFIER=$5
 
-  "${pub_script_root}/build-docs.sh" "${BUILD_DIR}"
+  "${pub_script_root}/build-docs.sh" "${BUILD_QUALIFIER}"
 
-  local zip_target="unified-docs-${BUILD_DIR}.zip"
-  echo "creating zip from built site"
-  pushd "${pub_script_root}/${BUILD_DIR}"
+  local zip_target="unified-docs${BUILD_QUALIFIER}.zip"
+  echo "creating zip from built site at /build${BUILD_QUALIFIER}"
+  pushd "${pub_script_root}/build${BUILD_QUALIFIER}"
   zip -r "/tmp/${zip_target}" .
   popd
 
@@ -45,12 +45,12 @@ echo "incoming branch named: $target_branch"
 if [ "${GIT_BRANCH:-}" == "${target_branch}" ]; then
   echo "========= on ${target_branch} branch - publishing to both main and staging"
   publish_docs "$STG_DOC_SSH_HOST" "$STG_DOC_SSH_PORT" \
-               "$STG_DOC_SSH_USER" "$STG_DOC_SSH_TARGET_DIR" "${pub_script_root}/build-stg"
+               "$STG_DOC_SSH_USER" "$STG_DOC_SSH_TARGET_DIR" "-stg"
   publish_docs "$PROD_DOC_SSH_HOST" "$PROD_DOC_SSH_PORT" \
-               "$PROD_DOC_SSH_USER" "$PROD_DOC_SSH_TARGET_DIR" "${pub_script_root}/build-prod"
+               "$PROD_DOC_SSH_USER" "$PROD_DOC_SSH_TARGET_DIR" "-prod"
 else
   echo "========= on ${target_branch} branch - publishing to staging only"
   publish_docs "$STG_DOC_SSH_HOST" "$STG_DOC_SSH_PORT" \
-               "$STG_DOC_SSH_USER" "$STG_DOC_SSH_TARGET_DIR" "${pub_script_root}/build-stg"
+               "$STG_DOC_SSH_USER" "$STG_DOC_SSH_TARGET_DIR" "-stg"
 fi
 rm "${pub_script_root}/github_deploy_key"
