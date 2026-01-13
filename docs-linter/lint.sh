fc8f149f85c3208b2ab97ef49f4a578b36195a7f
Nico Alba <nicoalba10@gmail.com>
2026-01-13 11:48:00 -0700

    remove comments

--- docs-linter/lint.sh
+++ docs-linter/lint.sh
@@ -26,7 +26,6 @@ MD_CLEAN=$(mktemp)
 # --- GATHER FILES ---
 echo "🎯 Gathering files from '$TARGET_DIR'..."
 
-# CHANGE 1: Added |_partials to the exclusion list to fix MD041 errors on snippets
 find "$TARGET_DIR" -type f \( -name "*.md" -o -name "*.mdx" \) \
     | grep -vE "/(node_modules|versioned_docs)/" \
     | grep -v "/_" \
@@ -102,7 +101,6 @@ fi
 if [ -s "$VALE_CLEAN" ]; then
     echo "####################### VALE REPORT #######################"
     cat "$VALE_CLEAN"
-    # If Markdownlint failed, remind the user right after Vale's "All Good" message
     if [ "$MD_ERR" -gt 0 ]; then
          echo "🛑 BUT WAIT! You also have $MD_ERR Markdownlint errors (see above)."
     fi
