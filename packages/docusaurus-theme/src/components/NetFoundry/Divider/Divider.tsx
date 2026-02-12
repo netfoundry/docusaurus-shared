9a9e03ffcfa9a9a1ace0234b8898cebdd06469c8
Nico Alba <nico.alba@netfoundry.io>
2026-02-12 21:38:42 +0000

    revert css changes unneeded

--- packages/docusaurus-theme/src/components/NetFoundry/Divider/Divider.tsx
+++ packages/docusaurus-theme/src/components/NetFoundry/Divider/Divider.tsx
@@ -3,14 +3,14 @@ import styles from './Divider.module.css';
 
 export function Divider(): JSX.Element {
     return (
-        <div className={styles.dividerConnector}>
-            <div className={styles.dividerDot} aria-hidden="true">
+        <div className={styles.onpremConnector}>
+            <div className={styles.onpremDot} aria-hidden="true">
                 <svg viewBox="0 0 24 24" role="img" aria-label="endpoint">
                     <circle cx="12" cy="12" r="8" fill="currentColor" />
                 </svg>
             </div>
-            <div className={styles.dividerLine} role="presentation" aria-hidden="true"></div>
-            <div className={styles.dividerDot} aria-hidden="true">
+            <div className={styles.onpremLine} role="presentation" aria-hidden="true"></div>
+            <div className={styles.onpremDot} aria-hidden="true">
                 <svg viewBox="0 0 24 24" role="img" aria-label="endpoint">
                     <circle cx="12" cy="12" r="8" fill="currentColor" />
                 </svg>
