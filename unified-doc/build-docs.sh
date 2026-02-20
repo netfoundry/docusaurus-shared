7ec82492dbf322b7e4148862c8648d92d123e7b4
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2026-02-20 13:59:28 -0500

    update gutter - hopefully

--- unified-doc/build-docs.sh
+++ unified-doc/build-docs.sh
@@ -285,7 +285,7 @@ if [ "${CLEAN:-0}" -eq 1 ]; then
 fi
 
 clone_or_update "https://bitbucket.org/netfoundry/zrok-connector.git"            frontdoor  develop
-clone_or_update "https://bitbucket.org/netfoundry/k8s-on-prem-installations.git" selfhosted update-redir
+clone_or_update "https://bitbucket.org/netfoundry/k8s-on-prem-installations.git" selfhosted main
 clone_or_update "https://github.com/openziti/ziti-doc.git"                       openziti   main
 clone_or_update "https://github.com/netfoundry/zlan.git"                         zlan       main
 clone_or_update "https://github.com/openziti/zrok.git"                           zrok       main
