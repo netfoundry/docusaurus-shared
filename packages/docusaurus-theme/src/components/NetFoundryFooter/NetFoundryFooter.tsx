8cf3d0f791de52fca24cb94797f5429462ee0c3b
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2026-03-28 21:14:32 -0400

    add x icon

--- packages/docusaurus-theme/src/components/NetFoundryFooter/NetFoundryFooter.tsx
+++ packages/docusaurus-theme/src/components/NetFoundryFooter/NetFoundryFooter.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import {NetFoundryHorizontalSection} from "../NetFoundryHorizontalSection";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from './styles.module.css';
+import {GitHubIcon, XIcon, YouTubeIcon} from '../icons';
 
 export interface SocialProps {
     twitterUrl?: string;
@@ -99,9 +100,7 @@ export function NetFoundryFooter(props: NetFoundryFooterProps) {
                             <div className={styles.footerSocialLinks}>
                                 {sp.githubUrl && (
                                     <a href={sp.githubUrl} target="_blank" className={styles.footerSocialLink} rel="noreferrer">
-                                        <svg width="20" height="20" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"/>
-                                        </svg>
+                                        <GitHubIcon width={20} height={20} />
                                     </a>
                                 )}
                                 {sp.linkedInUrl && (
@@ -115,16 +114,12 @@ export function NetFoundryFooter(props: NetFoundryFooterProps) {
                                 )}
                                 {sp.youtubeUrl && (
                                     <a href={sp.youtubeUrl} target="_blank" className={styles.footerSocialLink} rel="noreferrer">
-                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M15.969 4.69c-.183-1.358-1.062-2.275-2.361-2.46C12.371 2 8 2 8 2S3.629 2 2.362 2.229c-1.3.186-2.179 1.103-2.361 2.461C0 6.03 0 10 0 10s0 3.97.2 5.31c.183 1.358 1.062 2.275 2.361 2.46C3.63 18 8 18 8 18s4.371 0 5.638-.23c1.3-.185 2.178-1.102 2.361-2.46.2-1.34.2-5.31.2-5.31s0-3.97-.23-5.31zm-8.36, 8.57V6.73l3.76 2.27-3.76 2.26z"/>
-                                        </svg>
+                                        <YouTubeIcon width={16} height={16} />
                                     </a>
                                 )}
                                 {sp.twitterUrl && (
                                     <a href={sp.twitterUrl} target="_blank" className={styles.footerSocialLink} rel="noreferrer">
-                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                                        </svg>
+                                        <XIcon width={16} height={16} />
                                     </a>
                                 )}
                                 {sp.facebookUrl && (
